1a4459b71cac1f1ce4fd405cedbef7c601297b8c
Brian Lau <lauwangtatbrian@gmail.com>
2017-08-22 10:00:09 -0700

    add inital set-up for redux

---
+++ js/redux/combine-reducer.js
@@ -0,0 +1,15 @@
+// import { combineReducers } from 'redux';
+// import { reducer as formReducer } from 'redux-form';
+
+// import { SignUpReducer } from './modules/SignUp'
+// import { FormRedirectRenderer } from './modules/Form'
+// import { reviewReducer } from './modules/Reviews';
+// import { LogInReducer } from './modules/LogIn';
+
+// export default combineReducers({
+//   form: formReducer,
+//   review: reviewReducer,
+//   formRedirect: FormRedirectRenderer,
+//   registerUser: SignUpReducer,
+//   logIn: LogInReducer
+// })
---
+++ js/redux/store.js
@@ -0,0 +1,29 @@
+// import { createStore, applyMiddleware } from 'redux';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+// import { Tracker } from 'meteor/tracker';
+// import logger from 'redux-logger';
+// import combineReducers from './combine-reducers';
+
+
+// import { Forms } from '../../api/forms';
+
+// export default () => {
+//     const store = createStore(
+//                     combineReducers,
+//                     composeWithDevTools(
+//                         applyMiddleware(
+//                             logger
+//                         )
+//                     )
+//     );
+
+//     Tracker.autorun(() => {
+//         store.dispatch({
+//             type: 'LOAD_FORMS',
+//             form: Forms.find().fetch(),
+//         });
+//     });
+
+
+//     return store;
+// }
