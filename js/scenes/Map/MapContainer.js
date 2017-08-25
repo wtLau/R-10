05315b2c669130426d85da6ab1ec11fff0bd6179
Brian Lau <lauwangtatbrian@gmail.com>
2017-08-25 11:10:15 -0700

    anitmated lesson demo

--- js/scenes/Map/MapContainer.js
+++ js/scenes/Map/MapContainer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { ActivityIndicator } from 'react-native';
+import { ActivityIndicator, StyleSheet, View,LayoutAnimation, Animated ,TouchableOpacity, TouchableWithoutFeedback, Text} from 'react-native';
 
 import Map from './Map'
 
@@ -12,15 +12,96 @@ class MapContainer extends Component {
       }
     static propTypes = {}
 
+    constructor (props) {
+        super(props)
+        this.state = {
+            fontSize: 15,
+            translateValue: new Animated.ValueXY(),
+            color: new Animated.Value(0)
+        }
+    }
+
+    onPress = () => {
+        // LayoutAnimation.spring()
+        const animationConfig = {
+            duration: 3000,
+            update: {
+                type: 'spring',
+                springDamping: 0.1
+            }
+        }
+        LayoutAnimation.configureNext(animationConfig)
+        this.setState({ fontSize: this.state.fontSize*1.25})
+    }
+
+    animateCircle = () => {
+        this.state.translateValue.setValue({ x:0 , y:0 })
+        this.state.color.setValue(0)
+
+        Animated.parallel([
+            Animated.spring(
+                this.state.translateValue, 
+                { 
+                    toValue: { x: 51, y:51 }, 
+                    duration: 1000 
+                }
+            ),
+        Animated.timing(
+            this.state.color,
+            { 
+                toValue: 1, 
+                duration: 1000 
+            }
+        )
+        ]).start((animation) => {
+            if(animation.finished) {
+                this.animateCircle()
+            }
+        })
+    }
+
+
     render() {
-        // if (this.state.isLoading) {
-        //     return (
-        //         <ActivityIndicator animating={true} size="small" color="black" />
-        //     );
-        // } else {
-            return <Map />
-        // }
+        let animatedStyle = {
+            transform: [
+                { translateX: this.state.translateValue.x },
+                { translateY: this.state.translateValue.y }
+            ],
+            backgroundColor: color
+        }
+
+        let color = this.state.color.interpolate({
+            inputRange: [0, 1],
+            outputRange: ['blue', 'yellow']
+        })
+        return (
+            <View style={styles.container}>
+              <TouchableOpacity onPress={this.onPress}>
+                <Text style={{fontSize: this.state.fontSize}}>
+                  Welcome to React Native!
+                </Text>
+              </TouchableOpacity>
+              <TouchableWithoutFeedback onPress={this.animateCircle}>              
+                <Animated.View style={[ animatedStyle, styles.circle]} />
+              </TouchableWithoutFeedback>              
+            </View>
+          );
     }
 }
 
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#F5FCFF',
+    },
+    circle: {
+        borderRadius: 50,
+        height: 100,
+        width: 100,
+        backgroundColor: 'green'
+    }
+  });
+  
 export default MapContainer
