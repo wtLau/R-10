50cc05b427244b09aa0638fcebf18f2f41b764e6
Brian Lau <lauwangtatbrian@gmail.com>
2017-08-30 14:11:38 -0700

    replace sectinoList code by sectionList component

--- js/scenes/Schedule/Schedule.js
+++ js/scenes/Schedule/Schedule.js
@@ -1,47 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { 
-    Text, 
-    View,
-    SectionList,
-    TouchableOpacity,
-    Platform
-} from 'react-native';
-import { goToSession } from '../../lib/navigationHelpers'
-import { convertTimeHelper } from '../../lib/timeConvertHelper'
-import FaveHeart  from '../../components/FaveHeart/'
+import { View } from 'react-native';
 import { styles } from './styles'
-import  LineSeparator  from '../../components/LineSeparator/'
+import SectionListView from '../../components/SectionListView/'
 
 const Schedule = ({ data }) => (
     <View style={styles.container}>
-        <SectionList
-            renderSectionHeader= { 
-                (headerItem) => 
-                    <Text style={[styles.h3, styles.h3_time, styles.wrapper]}>
-                        {convertTimeHelper(headerItem.section.title)} </Text>}
-            renderItem={
-                ({item}) => 
-                    <View>
-                        <TouchableOpacity onPress={() =>goToSession('schedule', item)}>
-                            <View style={styles.p_wrapper}>
-                                <Text style={styles.h3}>
-                                    {item.title}
-                                    </Text>
-                                <Text style={styles.p}>
-                                    {item.location}
-                                    </Text> 
-                            </View>
-                        </TouchableOpacity>
-                        <View style={styles.faveHeart}>
-                            <FaveHeart/>
-                        </View>
-                    </View>
-                }
-            ItemSeparatorComponent = {()=> <LineSeparator/> }
-            keyExtractor={data => data.session_id}
-            sections={data}
-        />
+        <SectionListView data={data} pageOn='schedule'/>
     </View>
 )
 
--- js/scenes/Schedule/styles.js
+++ js/scenes/Schedule/styles.js
@@ -5,36 +5,4 @@ export const styles = {
       flex: 1,
       backgroundColor: colors.White,
     },
-
-    h3: {
-      fontSize: typography.baseSize,
-      fontWeight: '600',
-      fontFamily: typography.main,
-      paddingTop: 5,         
-      paddingBottom: 5,       
-    },
-    h3_time: {
-      paddingTop: 5,         
-      paddingBottom: 5,   
-      paddingLeft: 15,       
-    },
-    wrapper: {
-        backgroundColor: colors.LightGrey,
-    },
-    p_wrapper: {
-        paddingTop: 12,         
-        paddingBottom: 12,       
-        paddingLeft: 15,           
-    },
-    p: {
-        fontSize: typography.baseSize,
-        fontWeight: '600',
-        color: colors.MediumGrey,
-        fontFamily: typography.main,
-    },
-    faveHeart: {
-        position: 'absolute',
-        right: 10, 
-        bottom: 4,
-    }
   }
