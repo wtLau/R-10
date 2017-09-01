b38094cb17eaac2d3ae10ad68d151c83119f235f
Brian Lau <lauwangtatbrian@gmail.com>
2017-09-01 11:19:13 -0700

    add map with coordinate and marker

--- js/scenes/Map/Map.js
+++ js/scenes/Map/Map.js
@@ -1,21 +1,27 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import { 
-    Text, 
-    View,
-    Image,
-    FlatList
-} from 'react-native';
+
+import MapView from 'react-native-maps';
 
 import { styles } from './styles'
 
-const Map = ({ data }) => (
-    <View style={styles.container}>
-        <Text>This is Map</Text>
-    </View>
+const Map = () => (
+        <MapView
+            style={styles.map}
+            initialRegion={{
+            latitude: 49.2633479,
+            longitude: -123.1381278,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+            }}
+        >
+            <MapView.Marker draggable
+                coordinate={{
+                    latitude: 49.2633479,
+                    longitude: -123.1381278,
+                }}
+            />
+        </MapView>
 )
 
-
-Map.propTypes = {}
 
 export default Map
